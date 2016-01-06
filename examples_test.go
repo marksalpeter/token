ff377ef2dd8e428acac4abcf569bf577d7d75420
Mark Salpeter <marksalpeter@me.com>
2016-01-05 19:16:42 -0500

    added examples

---
+++ examples_test.go
@@ -0,0 +1,28 @@
+package token
+
+type Model struct {
+    ID	token.Token
+}
+
+
+func ExampleToken {
+	
+	model := Model {
+		ID:	token.New(),
+	}
+	var unmarshaled Model
+	marshaled, _ := json.Marshal(&model)
+	json.Unmarshal(marshaled, &unmarshaled)
+
+	fmt.Println(model.ID)
+	fmt.Println(model.ID.Encode())
+	fmt.Println(string(marshaled))
+	fmt.Println(uint64(unmarshaled.ID))
+	
+	// Output:
+	// 2751173559858
+	// Mr1NSSu
+	// {"ID":"Mr1NSSu"}
+	// 2751173559858
+	
+}
